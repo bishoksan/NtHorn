8dd4fa3985ddc00eae5ab9790efb0f693b916c63
bishoksan <bishoksan@gmail.com>
2023-04-27 23:34:44 +1000

    script minor change

--- nthorn_run_bench.sh
+++ nthorn_run_bench.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
 #set path to NtHorn here
-tool=/Users/bishoksankafle/imdea/NtHorn/src/nthorn
+tool=/Users/bishoksank/Research/progamAnalysis/softwares/NtHorn/src/nthorn
 #set of files to run are taken as the first argument
 Files=$1
-#please provide names for result files
 
+#please provide names for result files
 tmp=result_non-termination.txt
 out=res_nt-Horn.txt
+
+#for JSON output
+printf "{
+    "stats": {" >>$out
+
 START_G=$(date +%s000)
 for file in $Files/*.pl
 do
@@ -23,7 +28,14 @@ if [[ "$filename" == "*.c.map.t.pl" ]]; then #$file =~ \.gz$
   printf "$filename, " >>$out
 
   START_L=$(date +%s000)
+  #configuration cs
+  #gtimeout 5m $tool $file
+
+  #configuration cs.pe.clssplit
   gtimeout 5m $tool -pe -clssplit $file
+
+  #configuration cs.pe
+  #gtimeout 5m $tool -pe  $file
 status=$?
   END_L=$(date +%s000)
   DIFF_L=$(( $END_L - $START_L ))
@@ -40,6 +52,14 @@ done
 
 END_G=$(date +%s000)
 DIFF_G=$(( $END_G - $START_G ))
-printf  "Total time (ms): $DIFF_G\n" >>$out
+printf "},
+    "preamble": {
+        "benchmark": "my-benchmark-set",
+        "prog_args": "-a hello --arg2 world -v",
+        "program": "very_nice_solver",
+        "prog_alias": "vns"
+    }
+}" >>$out
+#printf  "Total time (ms): $DIFF_G\n" >>$out
 
 #rm $tmp
